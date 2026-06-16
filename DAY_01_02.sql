ba1baa958a5b70d774eccbb64e62046fda49f99b
Aachal Thakare <aachalthakare2001@gmail.com>
2026-06-16 20:24:53 +0530

    Add files via upload

---
+++ DAY_01_02.sql
@@ -0,0 +1,25 @@
+show databases;
+create database demo;
+show databases;
+drop database demo;
+drop database demos;
+show databases;
+show databases;
+create database demo;
+use demo;
+select database();
+create table student(
+name varchar(15),
+email varchar(15),
+Phone varchar(10)
+);
+show tables;
+desc tables;
+select *from student;
+create table demo.emp(
+name varchar(15),
+location varchar(20)
+);
+show tables;
+describe emp;
+select *from emp;
